b220b9744e71e09f47e4b1efba57718203c8a224
Umanga Chapagain <uchapaga@redhat.com>
2025-03-03 22:38:40 +0530

    read configuration from envvar or dotenv file as needed
    While running in-cluster deployments, some runtime parameters are
    provided as environment variables via Kubernetes downward-API and
    other schemes. But, while running out-of-cluster deployments, same
    schemes may not be possible. For such cases, this commit adds a
    capability to provide parameters as dotenv file. The dotenv file
    path should be provided as an argument during deployment.
    
    Running out-of-cluster is usually only preferred for developement
    environments and should not be used for production.
    
    Signed-off-by: Umanga Chapagain <uchapaga@redhat.com>

---
+++ controllers/utils/env.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnv(key string, filenames ...string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	envMap, err := godotenv.Read(filenames...)
+	if err != nil {
+		return ""
+	}
+	if value, ok := envMap[key]; ok {
+		return value
+	}
+	return ""
+}
+
+func GetEnvOrDefault(key, defaultValue string, filenames ...string) string {
+	if value := GetEnv(key, filenames...); value != "" {
+		return value
+	}
+	return defaultValue
+}
